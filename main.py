c8e84b03f9d84d15d6604629f6f2498f36328969
P4sTela <p4stela.exe@gmail.com>
2022-05-24 23:56:46 +0900

    update, add button

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 import SQLite as SQL
 from discord.ext import commands, tasks
-from dislash import InteractionClient, slash_commands, Option, OptionType
+from dislash import InteractionClient, slash_commands, Option, OptionType, ActionRow, Button, ButtonStyle
 from pprint import pprint
 from datetime import datetime
 import timetree as TT
@@ -193,6 +193,171 @@ async def miku(inter):
 #     embed = discord.Embed(title='今日の天気は...', description=tenki_text, color=0x8affff)
 #     tenki.clear()
 #     await inter.reply(embed=embed)
+
+
+@slash.command(name='vote', description='vote', guild_ids=guilds)
+async def vote(ctx):
+    row = ActionRow(
+        Button(
+            style=ButtonStyle.green,
+            label="A",
+            custom_id="A"
+        ),
+        Button(
+            style=ButtonStyle.red,
+            label="B",
+            custom_id="B"
+        )
+    )
+    msg = await ctx.send("投票", components=[row])
+    A = []
+    B = []
+    on_click = msg.create_click_listener(timeout=30)
+
+    @on_click.matching_id("A")
+    async def on_test_button(inter):
+        if (str(inter.author.id) in A) or (str(inter.author.id) in B):
+            await inter.reply("You are already voted!", ephemeral=True)
+
+        else:
+            A.append(str(inter.author.id))
+            await inter.reply("You've voted A", ephemeral=True)
+
+    @on_click.matching_id("B")
+    async def on_test_button(inter):
+        if (str(inter.author.id) in A) or (str(inter.author.id) in B):
+            await inter.reply("You are already voted!", ephemeral=True)
+
+        else:
+            B.append(str(inter.author.id))
+            await inter.reply("You've voted B", ephemeral=True)
+
+    @on_click.timeout
+    async def on_timeout():
+        embed = discord.Embed(
+            title="結果",
+            color=0x00ff00,
+        )
+        embed.set_author(name=ctx.author,
+                         icon_url=ctx.author.avatar_url  # Botのアイコンを設定してみる
+                         )
+        embed.add_field(name="A", value=str(len(A)))
+        embed.add_field(name="B", value=str(len(B)))
+        embed.set_footer(text="made by P4sTela",
+                         icon_url="https://raw.githubusercontent.com/P4sTela/P4sTela/main/iconmono_500.png")
+
+        await ctx.send(embed=embed)
+        await msg.edit(components=[])
+
+
+@slash.command(name='vote_id', description='vote add id', guild_ids=guilds)
+async def vote_id(ctx):
+    row = ActionRow(
+        Button(
+            style=ButtonStyle.green,
+            label="A",
+            custom_id="A"
+        ),
+        Button(
+            style=ButtonStyle.red,
+            label="B",
+            custom_id="B"
+        )
+    )
+    msg = await ctx.send("投票", components=[row])
+    A = []
+    B = []
+    on_click = msg.create_click_listener(timeout=30)
+
+    @on_click.matching_id("A")
+    async def on_test_button(inter):
+        if (str(inter.author.id) in A) or (str(inter.author.id) in B):
+            await inter.reply("You are already voted!", ephemeral=True)
+
+        else:
+            A.append(str(inter.author.id))
+            await inter.reply("You've voted A", ephemeral=True)
+
+    @on_click.matching_id("B")
+    async def on_test_button(inter):
+        if (str(inter.author.id) in A) or (str(inter.author.id) in B):
+            await inter.reply("You are already voted!", ephemeral=True)
+
+        else:
+            B.append(str(inter.author.id))
+            await inter.reply("You've voted B", ephemeral=True)
+
+    @on_click.timeout
+    async def on_timeout():
+        embed = discord.Embed(
+            title="結果",
+            color=0x00ff00,
+        )
+        embed.set_author(name=ctx.author,
+                         icon_url=ctx.author.avatar_url  # Botのアイコンを設定してみる
+                         )
+        embed.add_field(name="A", value=str(len(A)) + "\n" + "\n".join(A))
+        embed.add_field(name="B", value=str(len(B)) + "\n" + "\n".join(B))
+        embed.set_footer(text="made by P4sTela",
+                         icon_url="https://raw.githubusercontent.com/P4sTela/P4sTela/main/iconmono_500.png")
+
+        await ctx.send(embed=embed)
+        await msg.edit(components=[])
+
+
+@slash.command(name='vote_name', description='vote add name', guild_ids=guilds)
+async def vote_name(ctx):
+    row = ActionRow(
+        Button(
+            style=ButtonStyle.green,
+            label="A",
+            custom_id="A"
+        ),
+        Button(
+            style=ButtonStyle.red,
+            label="B",
+            custom_id="B"
+        )
+    )
+    msg = await ctx.send("投票", components=[row])
+    A = []
+    B = []
+    on_click = msg.create_click_listener(timeout=30)
+
+    @on_click.matching_id("A")
+    async def on_test_button(inter):
+        if (str(inter.author.name) in A) or (str(inter.author.name) in B):
+            await inter.reply("You are already voted!", ephemeral=True)
+
+        else:
+            A.append(str(inter.author.name))
+            await inter.reply("You've voted A", ephemeral=True)
+
+    @on_click.matching_id("B")
+    async def on_test_button(inter):
+        if (str(inter.author.name) in A) or (str(inter.author.name) in B):
+            await inter.reply("You are already voted!", ephemeral=True)
+
+        else:
+            B.append(str(inter.author.name))
+            await inter.reply("You've voted B", ephemeral=True)
+
+    @on_click.timeout
+    async def on_timeout():
+        embed = discord.Embed(
+            title="結果",
+            color=0x00ff00,
+        )
+        embed.set_author(name=ctx.author,
+                         icon_url=ctx.author.avatar_url  # Botのアイコンを設定してみる
+                         )
+        embed.add_field(name="A", value=str(len(A)) + "\n" + "\n".join(A))
+        embed.add_field(name="B", value=str(len(B)) + "\n" + "\n".join(B))
+        embed.set_footer(text="made by P4sTela",
+                         icon_url="https://raw.githubusercontent.com/P4sTela/P4sTela/main/iconmono_500.png")
+
+        await ctx.send(embed=embed)
+        await msg.edit(components=[])
 
 
 @slash.command(name='kaikei', guild_ids=guilds)
--- startup.sh
+++ startup.sh
@@ -12,14 +12,6 @@ stderr_logfile=/dev/stderr
 stderr_logfile_maxbytes=0
 autostart=true
 autorestart=true
-[program:python2]
-command=python /app/button.py 
-stdout_logfile=/dev/stdout
-stdout_logfile_maxbytes=0
-stderr_logfile=/dev/stderr
-stderr_logfile_maxbytes=0
-autostart=true
-autorestart=true
 ">/etc/supervisor/conf.d/supervisord.conf
 
 /usr/bin/supervisord
