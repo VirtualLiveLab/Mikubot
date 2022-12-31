609cd51453c9d4da41524fe760569911912fbf14
P4sTela <p4stela.exe@gmail.com>
2023-01-01 00:34:08 +0900

    back to nnomal

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ async def on_ready():
     print('Done Login')
     ch_id = 938738283364618264  # general
     chennel = client.get_channel(ch_id)
-    embed = discord.Embed(description="あけましておめでとうございます!\nミクが起動したよ!", color=0x66DDCC)
-    await chennel.send(embed=embed)
+    embed = discord.Embed(description="ミクが起動したよ!", color=0x66DDCC)
+    # await chennel.send(embed=embed)
     await client.change_presence(activity=discord.Game('プロセカ'))
 
 
@@ -100,7 +100,7 @@ async def omikuji(inter):
     elif ran < 0.6:
         txt = "中吉！"
     elif ran < 0.8:
-        txt = "小吉"
+        txt = "小吉！"
     elif ran < 0.99:
         txt = "末吉！"
     else:
