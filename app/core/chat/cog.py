6383f85cfa4e64a9d73473e3cdcefff0b09cd808
sushi-chaaaan <mail@sushichan.live>
2023-09-25 20:45:54 +0900

    feat: implement message extraction draft

--- app/core/chat/cog.py
+++ app/core/chat/cog.py
@@ -3,11 +3,11 @@ from typing import TYPE_CHECKING
 import discord
 from discord import app_commands
 from discord.ext import commands
-from dispander import dispand
 
-from app.core.chat import embed
-from components.ui import LinkButton
-from const.enums import Color
+from utils.extract import MessageExtractor
+
+from .embed import process_message_to_embeds, user_embed
+from .view import DispandView
 
 if TYPE_CHECKING:
     # import some original class
@@ -47,7 +47,7 @@ class Chat(commands.Cog):
     @app_commands.command(name="helloworld", description="Hello World!")  # type: ignore[arg-type]
     async def hello_world(self, interaction: discord.Interaction) -> None:
         await interaction.response.defer(ephemeral=False)
-        emb = embed.user_embed(interaction.user)
+        emb = user_embed(interaction.user)
         await interaction.followup.send(embed=emb)
 
     @commands.Cog.listener("on_message")
@@ -55,25 +55,14 @@ class Chat(commands.Cog):
         if message.author.id == self.bot.user.id:  # type: ignore[union-attr]
             return
 
-        try:
-            extracted = await dispand(message, with_reference=False, accent_color=Color.MIKU)
-        except Exception:
-            self.bot.logger.exception("dispand error")
-            extracted = []
+        extractor = MessageExtractor(self.bot)
+        extracted_messages = await extractor.extract_from_message(message=message)
 
-        if extracted is None or extracted == []:
-            return
-
-        for fragment in extracted:
-            view = discord.ui.View(timeout=None)
-            view.add_item(
-                LinkButton("元のメッセージ", url=fragment["jump_url"]),
-            )
-
+        for msg in extracted_messages:
             try:
                 await message.channel.send(
-                    embeds=fragment["embeds"][:10],
-                    view=view,
+                    embeds=process_message_to_embeds(msg),
+                    view=DispandView(message_url=msg.jump_url),
                 )
             except Exception:
                 self.bot.logger.exception("dispand error")
