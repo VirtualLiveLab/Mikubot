6bb2e5164206e3d894e51c17128a84d63f008c79
sushi-chaaaan <mail@sushichan.live>
2023-09-25 20:35:53 +0900

    refactor: only do message extraction in utils

--- utils/extract.py
+++ utils/extract.py
@@ -1,10 +1,9 @@
 import re
-from typing import TypedDict
 
 import discord
 from discord import Client, Colour, Embed, Message
 
-from const.discord import MAX_EMBEDS_PER_MESSAGE
+from .finder import Finder
 
 regex_discord_message_url = (
     "(?!<)https://(ptb.|canary.)?discord(app)?.com/channels/"
@@ -15,60 +14,72 @@ regex_extra_url = (
 )
 
 
-class ExtractedMessage(TypedDict):
-    guild_id: int
-    channel_or_thread_id: int
-    message_id: int
-    jump_url: str
-    embeds: list[Embed]
-
-
-class MessageDispander:
+class MessageExtractor:
     def __init__(self, client: Client) -> None:
         self._client = client
 
-    async def dispand(self, *, message: discord.Message) -> list[ExtractedMessage]:
-        if not message.guild:
+    async def extract_from_message(self, *, message: discord.Message) -> list[Message]:
+        if message.guild is None:
             return []
-        # mock message
-        return [
-            {
-                "channel_or_thread_id": message.channel.id,
-                "guild_id": message.guild.id,
-                "message_id": message.id,
-                "jump_url": message.jump_url,
-                "embeds": [compose_embed(message, with_reference=True, accent_color=True)],
-            },
-        ]
+
+        # extract messages
+        return await self._extract_from_message(message)
+
+    async def _extract_from_message(self, message: Message) -> list[Message]:
+        messages: list[Message] = []
+        for ids in re.finditer(regex_discord_message_url, message.content):
+            if message.guild is None:
+                continue
+            fetched_message = await self._fetch_message_from_id(
+                guild_id=int(ids["guild"]),
+                channel_id=int(ids["channel"]),
+                message_id=int(ids["message"]),
+            )
+            if fetched_message is not None:
+                messages.append(fetched_message)
+        return messages
+
+    async def _fetch_message_from_id(self, guild_id: int, channel_id: int, message_id: int) -> Message | None:
+        finder = Finder(self._client)
+        await finder.find_guild(guild_id)
+
+        channel = await finder.find_channel(channel_id)
+        if not isinstance(channel, discord.TextChannel | discord.VoiceChannel | discord.Thread):
+            return None
+
+        try:
+            return await channel.fetch_message(message_id)
+        except Exception:  # noqa: BLE001
+            return None
 
 
-async def dispand(*, message: Message, with_reference: bool = True, accent_color: int = 0x3498DB) -> list[ExtractedMessage]:
-    messages = await extract_message(message)
-    extracted: list[ExtractedMessage] = []
-    for msg in messages:
-        embeds = []
-        if msg.content or msg.attachments:
-            embeds.append(compose_embed(msg, with_reference=with_reference, accent_color=accent_color))
-        # Send the second and subsequent attachments with embed (named 'embed') respectively:
-        for attachment in msg.attachments[1:]:
-            embed = Embed()
-            embed.set_image(url=attachment.proxy_url)
-            embeds.append(embed)
+# async def dispand(*, message: Message, with_reference: bool = True, accent_color: int = 0x3498DB):
+#     messages = await extract_message(message)
+#     extracted: list[ExtractedMessage] = []
+#     for msg in messages:
+#         embeds = []
+#         if msg.content or msg.attachments:
+#             embeds.append(compose_embed(msg, with_reference=with_reference, accent_color=accent_color))
+#         # Send the second and subsequent attachments with embed (named 'embed') respectively:
+#         for attachment in msg.attachments[1:]:
+#             embed = Embed()
+#             embed.set_image(url=attachment.proxy_url)
+#             embeds.append(embed)
 
-        # add msg.embeds until embeds reaches 10
-        for embed in msg.embeds:
-            if len(embeds) >= MAX_EMBEDS_PER_MESSAGE:
-                break
-            embeds.append(embed)
+#         # add msg.embeds until embeds reaches 10
+#         for embed in msg.embeds:
+#             if len(embeds) >= MAX_EMBEDS_PER_MESSAGE:
+#                 break
+#             embeds.append(embed)
 
-        # extracted.append(
-        #     {
-        #         "id": msg.id,
-        #         "jump_url": msg.jump_url,
-        #         "embeds": embeds,
-        #     },
-        # )
-    return extracted
+#         # extracted.append(
+#         #     {
+#         #         "id": msg.id,
+#         #         "jump_url": msg.jump_url,
+#         #         "embeds": embeds,
+#         #     },
+#         # )
+#     return extracted
 
 
 async def extract_message(message: Message) -> list[Message]:
