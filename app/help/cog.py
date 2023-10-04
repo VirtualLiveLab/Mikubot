78b8a377c788b3eaac85886cceabf213ecb40b8f
sushi-chaaaan <mail@sushichan.live>
2023-10-04 11:49:06 +0900

    style: split view and cog

--- app/help/cog.py
+++ app/help/cog.py
@@ -4,12 +4,10 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from ductile import State, View, ViewObject
 from ductile.controller import InteractionController
-from ductile.ui import Select, SelectOption
 
-from .const import FEATURE_LABEL_LIST, FeatureLabel
-from .embed import get_help_embed
+from .const import FeatureLabel
+from .view import HelpView
 
 if TYPE_CHECKING:
     # import some original class
@@ -34,39 +32,6 @@ class Help(commands.Cog):
             feature_name = "ヘルプ"
         controller = InteractionController(HelpView(command_name=feature_name), interaction=interaction)
         await controller.send()
-
-
-class HelpView(View):
-    def __init__(self, command_name: FeatureLabel) -> None:
-        super().__init__()
-        self.current = State[FeatureLabel](command_name, self)
-
-    def render(self) -> ViewObject:
-        async def on_select(interaction: discord.Interaction, values: list[str]) -> None:
-            await interaction.response.defer(ephemeral=True)
-            if (selected := values[0]) not in FEATURE_LABEL_LIST:
-                selected = "ヘルプ"
-            self.current.set_state(selected)
-
-        return ViewObject(
-            embeds=[
-                get_help_embed(self.current()),
-            ],
-            components=[
-                Select(
-                    config={
-                        "max_values": 1,
-                    },
-                    style={
-                        "placeholder": "使い方を見たい機能を選択してください。",
-                    },
-                    options=[
-                        SelectOption(label=n, value=n, selected_by_default=n == self.current()) for n in FEATURE_LABEL_LIST
-                    ],
-                    on_select=on_select,
-                ),
-            ],
-        )
 
 
 async def setup(bot: "Bot") -> None:
---
+++ app/help/view.py
@@ -0,0 +1,40 @@
+import discord
+
+from ductile import State, View, ViewObject
+from ductile.ui import Select, SelectOption
+
+from .const import FEATURE_LABEL_LIST, FeatureLabel
+from .embed import get_help_embed
+
+
+class HelpView(View):
+    def __init__(self, command_name: FeatureLabel) -> None:
+        super().__init__()
+        self.current = State[FeatureLabel](command_name, self)
+
+    def render(self) -> ViewObject:
+        async def on_select(interaction: discord.Interaction, values: list[str]) -> None:
+            await interaction.response.defer(ephemeral=True)
+            if (selected := values[0]) not in FEATURE_LABEL_LIST:
+                selected = "ヘルプ"
+            self.current.set_state(selected)
+
+        return ViewObject(
+            embeds=[
+                get_help_embed(self.current()),
+            ],
+            components=[
+                Select(
+                    config={
+                        "max_values": 1,
+                    },
+                    style={
+                        "placeholder": "使い方を見たい機能を選択してください。",
+                    },
+                    options=[
+                        SelectOption(label=n, value=n, selected_by_default=n == self.current()) for n in FEATURE_LABEL_LIST
+                    ],
+                    on_select=on_select,
+                ),
+            ],
+        )
