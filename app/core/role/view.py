07573f5ca68c665f7476c731f8bd2746d3711979
sushi-chaaaan <mail@sushichan.live>
2023-10-21 03:27:35 +0900

    Revert "feat: add pagination to role check"
    This reverts commit cb172b44fd4b340657a587075fe606ade1b4f26e.

--- app/core/role/view.py
+++ app/core/role/view.py
@@ -1,7 +1,8 @@
-from discord import Embed, Interaction, Role, ui
+from discord import Embed, Interaction, Role
 from ductile import State, View, ViewObject
-from ductile.ui import Button, RoleSelect
+from ductile.ui import RoleSelect
 
+from const.discord import MAX_EMBED_FIELD_VALUE_LENGTH
 from const.enums import Color
 
 
@@ -9,8 +10,6 @@ class RoleCheckView(View):
     def __init__(self) -> None:
         super().__init__()
         self.selected = State[Role | None](None, self)
-        self.current_page = State[int](0, self)
-        self.max_page = 0
 
     def render(self) -> ViewObject:
         async def handle_select(interaction: Interaction, values: list[Role]) -> None:
@@ -25,12 +24,9 @@ class RoleCheckView(View):
             )
 
         def role_embed(role: Role) -> Embed:
-            # chunk role members by 20
-            chunked_members = [role.members[i : i + 20] for i in range(0, len(role.members), 20)]
-            self.max_page = len(chunked_members) - 1
-
-            def get_page_value() -> str:
-                return "\n".join([m.mention for m in chunked_members[self.current_page()]])
+            value = "\n".join([m.mention for m in role.members])
+            if len(value) > MAX_EMBED_FIELD_VALUE_LENGTH:
+                value = f"{value[:MAX_EMBED_FIELD_VALUE_LENGTH - 3]}..."
 
             e = Embed(
                 title="ロール概要",
@@ -38,42 +34,20 @@ class RoleCheckView(View):
                 color=Color.MIKU,
             )
             e.add_field(
-                name=f"このロールを持つメンバー({self.current_page() + 1}/{len(chunked_members)})",
-                value=get_page_value(),
+                name=f"このロールを持つメンバー({len(role.members)}人)",
+                value=value,
             )
             return e
 
-        components: list[ui.Item] = [
-            RoleSelect(
-                config={"min_values": 1, "max_values": 1},
-                style={
-                    "placeholder": "確認したいロールを選択してください。",
-                },
-                on_select=handle_select,
-            ),
-        ]
-        if self.max_page > 1:
-            components += [
-                Button(
-                    "<",
-                    style={"color": "grey", "disabled": self.current_page() == 0},
-                    on_click=lambda _: self.current_page.set_state(lambda x: x - 1),
-                ),
-                Button(
-                    f"{self.current_page() + 1}/{self.max_page + 1}",
-                    style={"color": "grey", "disabled": True},
-                ),
-                Button(
-                    ">",
-                    style={
-                        "color": "grey",
-                        "disabled": self.current_page() == self.max_page,
-                    },
-                    on_click=lambda _: self.current_page.set_state(lambda x: x + 1),
-                ),
-            ]
-
         return ViewObject(
             embeds=[default_embed() if (r := self.selected()) is None else role_embed(r)],
-            components=components,
+            components=[
+                RoleSelect(
+                    config={"min_values": 1, "max_values": 1},
+                    style={
+                        "placeholder": "確認したいロールを選択してください。",
+                    },
+                    on_select=handle_select,
+                ),
+            ],
         )
