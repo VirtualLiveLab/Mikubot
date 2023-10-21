d5d4d72096505092ae24250b13acd2165862be7a
sushi-chaaaan <mail@sushichan.live>
2023-10-21 18:24:07 +0900

    refactor: migrate into `src` directory

---
+++ src/app/core/role/cog.py
@@ -0,0 +1,36 @@
+from typing import TYPE_CHECKING
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+from ductile.controller import InteractionController
+
+from .view import RoleCheckView
+
+if TYPE_CHECKING:
+    # import some original class
+    from src.app.bot import Bot
+
+    pass
+
+
+class Role(commands.Cog):
+    role = app_commands.Group(name="role", description="ロール関連のコマンド")
+
+    def __init__(self, bot: "Bot") -> None:
+        self.bot = bot
+
+    @role.command(name="check", description="ロールを確認します。")
+    async def check_role(self, interaction: discord.Interaction) -> None:
+        await interaction.response.defer(ephemeral=True)
+        controller = InteractionController(
+            RoleCheckView(),
+            interaction=interaction,
+            timeout=None,
+            ephemeral=True,
+        )
+        await controller.send()
+
+
+async def setup(bot: "Bot") -> None:
+    await bot.add_cog(Role(bot))
---
+++ src/app/help/embed.py
@@ -0,0 +1,109 @@
+from discord import Embed
+
+from src.const.enums import Color
+
+from .const import FeatureLabel
+
+
+def get_help_embed(name: FeatureLabel) -> Embed:
+    d: dict[FeatureLabel, Embed] = {
+        # "部費ロール": buhi_embed(),
+        "ヘルプ": help_embed(),
+        "その他": other_embed(),
+        "投票": vote_embed(),
+        "ピン留め": pin_embed(),
+        "メッセージ展開": dispand_embed(),
+    }
+    return d[name]
+
+
+def _base_embed() -> Embed:
+    return Embed(color=Color.MIKU)
+
+
+def buhi_embed() -> Embed:
+    return _base_embed()
+
+
+def help_embed() -> Embed:
+    e = _base_embed()
+    e.title = "`/help`"
+    e.description = "コマンドの使い方(これ)を表示します。"
+    return e
+
+
+def other_embed() -> Embed:
+    e = _base_embed()
+    e.title = "その他の機能"
+    e.add_field(
+        name="`/helloworld`",
+        value="Hello World!を表示します。",
+    )
+    e.add_field(
+        name="`/miku`",
+        value="ミクさんが返事をしてくれるよ！",
+    )
+    return e
+
+
+def vote_embed() -> Embed:
+    e = _base_embed()
+    e.title = "`/vote`"
+    e.description = "投票を作成します。"
+    e.add_field(
+        name="使い方(2択)",
+        value="""
+質問文のみを指定すると自動的にはい/いいえの2択の投票が作成されます。
+例: `/vote あなたはVLL部員ですか？`
+""",
+        inline=False,
+    )
+    e.add_field(
+        name="使い方(3択以上)",
+        value="""
+質問文を入力したあと、続けて選択肢を入力してください。
+最大で20個まで入力できます。
+例: `/vote 今日の晩御飯は？ ラーメン うどん そば`
+""",
+        inline=False,
+    )
+    e.add_field(
+        name="使い方(集計)",
+        value="""
+`/vote`コマンドを実行すると、投票の詳細とリアクションがついたメッセージが送信されます。
+集計するときはそのメッセージを右クリック(PC) or 長押し(スマホ)して、
+「投票を集計」を選択してください。
+""",
+        inline=False,
+    )
+    return e
+
+
+def pin_embed() -> Embed:
+    e = _base_embed()
+    e.title = "ピン留め"
+    e.description = "メッセージをピン留め(もしくは解除)します。"
+    e.add_field(
+        name="使い方",
+        value="""
+メッセージを右クリック(PC) or 長押し(スマホ)して、
+「Pin / Unpin」を選択してください。
+
+まだピン留めされていないメッセージを選択するとピン留めされ、
+ピン留めされたメッセージを選択するとピン留めが解除されます。
+""",
+        inline=False,
+    )
+    return e
+
+
+def dispand_embed() -> Embed:
+    e = _base_embed()
+    e.title = "メッセージ展開"
+    e.description = "メッセージURLを展開します。"
+    e.add_field(
+        name="使い方",
+        value="Mikubotが導入されているサーバー内かつ__**閲覧権限がある**__チャンネルのメッセージURLを送信すると、埋め込みで展開されます。",
+        inline=False,
+    )
+    return e
