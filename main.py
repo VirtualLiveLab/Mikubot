25bd8f53acabb998b8d936a743685f72bb2e09e0
sushi-chaaaan <mail@sushichan.live>
2023-10-21 18:24:07 +0900

    refactor: migrate into `src` directory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-from app.bot import Bot
+from src.app.bot import Bot
 
 if __name__ == "__main__":
     bot = Bot()
---
+++ src/app/core/role/view.py
@@ -0,0 +1,53 @@
+from discord import Embed, Interaction, Role
+from ductile import State, View, ViewObject
+from ductile.ui import RoleSelect
+
+from src.const.discord import MAX_EMBED_FIELD_VALUE_LENGTH
+from src.const.enums import Color
+
+
+class RoleCheckView(View):
+    def __init__(self) -> None:
+        super().__init__()
+        self.selected = State[Role | None](None, self)
+
+    def render(self) -> ViewObject:
+        async def handle_select(interaction: Interaction, values: list[Role]) -> None:
+            await interaction.response.defer()
+            self.selected.set_state(values[0])
+
+        def default_embed() -> Embed:
+            return Embed(
+                title="ロール概要",
+                description="見たいロールを選択してください。",
+                color=Color.MIKU,
+            )
+
+        def role_embed(role: Role) -> Embed:
+            value = "\n".join([m.mention for m in role.members])
+            if len(value) > MAX_EMBED_FIELD_VALUE_LENGTH:
+                value = f"{value[:MAX_EMBED_FIELD_VALUE_LENGTH - 3]}..."
+
+            e = Embed(
+                title="ロール概要",
+                description=f"{role.mention}の概要を表示しています。",
+                color=Color.MIKU,
+            )
+            e.add_field(
+                name=f"このロールを持つメンバー({len(role.members)}人)",
+                value=value,
+            )
+            return e
+
+        return ViewObject(
+            embeds=[default_embed() if (r := self.selected()) is None else role_embed(r)],
+            components=[
+                RoleSelect(
+                    config={"min_values": 1, "max_values": 1},
+                    style={
+                        "placeholder": "確認したいロールを選択してください。",
+                    },
+                    on_select=handle_select,
+                ),
+            ],
+        )
