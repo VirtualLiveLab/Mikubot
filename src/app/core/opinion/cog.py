0757c9ce6108d88b39d3c81a43a4647f76f7c4f0
sushi-chaaaan <mail@sushichan.live>
2023-12-12 13:42:12 +0900

    feat: add opinion command

---
+++ src/app/core/opinion/cog.py
@@ -0,0 +1,51 @@
+import os
+from traceback import print_exc
+
+import discord
+from app.core.opinion.view import OpinionModal
+from const.enums import Color
+from discord import Embed, app_commands
+from discord.ext import commands
+from utils.finder import Finder
+
+
+class Opinion(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @app_commands.command(name="opinion", description="サークル運営についての意見を送信するよ！")
+    @app_commands.guilds(discord.Object(id=int(os.environ["GUILD_ID"])))
+    @app_commands.guild_only()
+    async def opinion(self, interaction: discord.Interaction) -> None:
+        await interaction.response.send_modal(OpinionModal(self.on_submit))
+
+    async def on_submit(self, interaction: discord.Interaction, values: dict[str, str]) -> None:
+        await interaction.response.defer(ephemeral=True)
+        opinion = values.get("意見", "")
+
+        try:
+            embed = Embed(
+                title="意見が届きました！",
+                description=opinion,
+                color=Color.MIKU,
+            )
+            # replace 0 with channel id
+            channel = await Finder(self.bot).find_channel(
+                int(os.environ["OPINION_CHANNEL_ID"]), expected_type=discord.TextChannel
+            )
+            await channel.send(embed=embed)
+        except Exception:  # noqa: BLE001
+            print_exc()
+            e = Embed(
+                title="エラーが発生しました。",
+                description="送信に失敗しました。\n何度も続く場合は管理者に連絡してください。",
+                color=Color.MIKU,
+            )
+        else:
+            e = Embed(title="送信完了！", description="貴重なご意見ありがとうございます！", color=Color.MIKU)
+
+        await interaction.edit_original_response(embed=e)
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(Opinion(bot))
