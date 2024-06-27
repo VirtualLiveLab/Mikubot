88e956694cda73ec32b0cd574a1c2aca355ed59b
sushi-chaaaan <mail@sushichan.live>
2024-06-28 00:10:22 +0900

    タイムスタンプ ユーティリティの追加 Fixes #93

--- src/utils/timestamp.py
+++ src/utils/timestamp.py
@@ -15,12 +15,45 @@ class DiscordTimestampStyle(StrEnum):
 
 
 class DiscordTimestamp:
+    """
+    Utility class for Discord timestamp.
+
+    ref: https://discord.com/developers/docs/reference#message-formatting-timestamp-styles
+    """
+
     def __init__(self, posix_timestamp: str) -> None:
         self.posix_timestamp = posix_timestamp
 
     @classmethod
     def from_datetime(cls, dt: datetime, tz: timezone) -> Self:
+        """
+        initialize with datetime object.
+
+        Parameters
+        ----------
+        dt : `datetime.datetime`
+        tz : `datetime.timezone`
+
+        Returns
+        -------
+        Self
+            DiscordTimestamp object.
+        """
         return cls(str(int(dt.astimezone(tz).timestamp())))
 
-    def to_discord_timestamp(self, style: DiscordTimestampStyle = DiscordTimestampStyle.LONG_DATE_WITH_SHORT_TIME) -> str:
+    def export_with_style(self, style: DiscordTimestampStyle = DiscordTimestampStyle.LONG_DATE_WITH_SHORT_TIME) -> str:
+        """
+        export timestamp with style for sending to Discord.
+        default style is `LONG_DATE_WITH_SHORT_TIME`.
+
+        Parameters
+        ----------
+        style : `DiscordTimestampStyle`, optional
+            Timestamp Style. by default DiscordTimestampStyle.LONG_DATE_WITH_SHORT_TIME
+
+        Returns
+        -------
+        str
+            formatted timestamp string.
+        """
         return f"<t:{self.posix_timestamp}:{style}>"
